5795d2e3f45863b79b542e51407bb4057aca8dc2
Karolis Davli <kdavlijevas@gmail.com>
2020-10-10 19:41:33 +0100

    Loop added

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 // Global variables
-const computerSelection = computerPlay();
-const playerSelection = playerPlay();
-// Score counters
-let playerScore = 0;
-let computerScore = 0;
-let draw = 0;
+let computerSelection;
+let playerSelection;
 
 // Player picks an option
 function playerPlay() {
   let playerInput = prompt("Enter you selection: 'rock - paper - scissors' ");
   // Format players input
-  let playerChoice = playerInput.charAt(0).toUpperCase() + playerInput.substring(1, playerInput.length).toLowerCase();
+  let playerChoice =
+    playerInput.charAt(0).toUpperCase() +
+    playerInput.substring(1, playerInput.length).toLowerCase();
   return playerChoice;
 }
 
 // Computer randomly picks an option
 function computerPlay() {
-  let game = ['Rock', 'Paper', 'Scissors'];
-  let i = Math.floor((Math.random() * 2));
+  let game = ["Rock", "Paper", "Scissors"];
+  let i = Math.floor(Math.random() * 2);
   let computerChoice = game[i];
   return computerChoice;
 }
-
 
 // Game play
 function playRound(playerSelection, computerSelection) {
@@ -29,16 +26,32 @@ function playRound(playerSelection, computerSelection) {
   if (computerSelection === playerSelection) {
     return "Tie";
     // Check if player wins
-  } else if (computerSelection == "Rock" && playerSelection == "Paper" ||
-    computerSelection == "Scissors" && playerSelection == "Rock" ||
-    computerSelection == "Paper" && playerSelection == "Scissors") {
+  } else if (
+    (computerSelection == "Rock" && playerSelection == "Paper") ||
+    (computerSelection == "Scissors" && playerSelection == "Rock") ||
+    (computerSelection == "Paper" && playerSelection == "Scissors")
+  ) {
     console.log(`${playerSelection} beats ${computerSelection}, Player Wins!`);
     return "You Win!";
     // Check if computer wins
   } else {
-    console.log(`${computerSelection} beats ${playerSelection}, Computer Wins!`);
+    console.log(
+      `${computerSelection} beats ${playerSelection}, Computer Wins!`
+    );
     return "You Lose!";
   }
 }
 
-playRound(playerSelection, computerSelection);
+function game() {
+  for (i = 0; i < 5; i++) {
+    computerSelection = computerPlay();
+    playerSelection = playerPlay();
+    playRound(playerSelection, computerSelection);
+
+    console.log("Player selection", playerSelection);
+    console.log("Computer selection", computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+
+game();
